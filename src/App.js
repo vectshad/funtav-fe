cb19ebbcf5a6f3f93e0815838f3563d5a6ea15c6
ekasunandika <ekasunandikaa@gmail.com>
2022-01-04 18:50:24 +0700

    show packages on home page

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ import './App.css';
 
 function App() {
     return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />}/>
-				<Route path="/login" element={<Login />}/>
-				<Route path="/register" element={<Register />}/>
-				<Route path="/mypackages" element={<MyPackages />}/>
-				<Route path="/custompackage" element={<CustomPackage />}/>
-				<Route path="/about" element={<AboutUs />}/>
-				<Route path="/:id" element={<PackageDetail />}/>
-			</Routes>
-		</Router>
+		<div className='App'>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />}/>
+					<Route path="/login" element={<Login />}/>
+					<Route path="/register" element={<Register />}/>
+					<Route path="/mypackages" element={<MyPackages />}/>
+					<Route path="/custompackage" element={<CustomPackage />}/>
+					<Route path="/about" element={<AboutUs />}/>
+					<Route path="/:id" element={<PackageDetail />}/>
+				</Routes>
+			</Router>
+		</div>
     );
 }
 
