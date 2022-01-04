6adc13daed8227562e55daae32712cfa746981cd
ekasunandika <ekasunandikaa@gmail.com>
2022-01-04 10:23:56 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import MyPackages from "./components/MyPackages";
+import CustomPackage from "./components/CustomPackage";
+import AboutUs from "./components/AboutUs";
+import PackageDetail from "./components/PackageDetail";
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-			test
-        </div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />}/>
+				<Route path="/login" element={<Login />}/>
+				<Route path="/register" element={<Register />}/>
+				<Route path="/mypackages" element={<MyPackages />}/>
+				<Route path="/custompackage" element={<CustomPackage />}/>
+				<Route path="/about" element={<AboutUs />}/>
+				<Route path="/:id" element={<PackageDetail />}/>
+			</Routes>
+		</Router>
     );
 }
 
