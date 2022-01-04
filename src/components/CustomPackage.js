522c5364a84d643342891a87ca90c3742c6aaa88
ekasunandika <ekasunandikaa@gmail.com>
2022-01-04 09:45:34 +0700

    initial commit

---
+++ src/components/CustomPackage.js
@@ -0,0 +1,11 @@
+import React, { useImperativeHandle } from 'react'
+
+function CustomPackage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CustomPackage
---
+++ src/components/PackageDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PackageDetail() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PackageDetail
