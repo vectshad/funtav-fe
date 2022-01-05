3bd60503305aea45529a44aa96472fba213c280c
ekasunandika <ekasunandikaa@gmail.com>
2022-01-05 12:17:32 +0700

    add About Us page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Navbar from './Navbar';
+import "../styles/AboutUs.css";
 
 function AboutUs() {
     return (
         <div>
-            
+            <Navbar/>
+            <h1 className='mb-5 mt-3'>About Us</h1>
+            <div className='About'>
+                <p>
+                Fun Tav is specialized on making custom tour and travel inquiries based on customer’s. Mr. Vijay is the owner of FunTav, a travel and tour companies which has been established since 1998, based on Indonesia. Fun Tav provides several tour packages that are already available. You can make custom tour packages according to your wishes.
+                </p>
+            </div>
+            <small class="text-muted">Fun Tav</small>
         </div>
     )
 }
